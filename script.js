6ff8cbd3dd31cc4e2573c9b49b51be719a26d06f
Nour-Alkhateeb <noralakateb711@gmail.com>
2022-11-11 22:34:08 +0300

    first html.index

---
+++ script.js
@@ -0,0 +1,12 @@
+let output=document.getElementById('output');
+
+let result
+
+function display(num){output.value +=num;}
+
+function calc(){result=eval(output.value);
+output.value=result;}
+
+
+
+
