b1e46a60df620f4497e37dca7bcd6a727c357adc
Nour-Alkhateeb <noralakateb711@gmail.com>
2022-11-14 23:42:51 +0300

    adding conditions to creat sanity checks for user inputs to enure a correct mathmaticial operations

--- script.js
+++ script.js
@@ -2,11 +2,32 @@ let output=document.getElementById('output');
 
 let result
 
-function display(num){output.value +=num;}
+let operationLatchFlag = 0;
+let dotLatchFlag=0;
 
-function calc(){result=eval(output.value);
-output.value=result;}
+function display(num){
+    if ((num ===".") && (dotLatchFlag===0))
+    { output.value +=num;  
+        dotLatchFlag=1;
+    }
+    else if((num === '+' || num === '*' || num === '/' ) && (operationLatchFlag === 0) ){
+        output.value +=num;   
+        operationLatchFlag = 1;
+        dotLatchFlag=0;
+    }
+    else if(num != '+' && num != '*' && num != '/'&& num!='.' ){
+        output.value +=num;
+        operationLatchFlag = 0;
+    }  
+}
 
+function cleardisplay(){
+    output.value = ''
+    operationLatchFlag = 0;
+dotLatchFlag=0;}
 
-
-
+function calc(){
+    result=eval(output.value);
+    output.value=result;
+    operationLatchFlag = 0;
+}
